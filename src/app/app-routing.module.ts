08798618006305903ee0f3ad607634876bc1a1bc
Guido Pontet <guidopontet@gmail.com>
2019-07-10 21:29:31 -0300

    Initialize empty app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'alert', loadChildren: './pages/alert/alert.module#AlertPageModule' },
+  { path: 'action-sheet', loadChildren: './pages/action-sheet/action-sheet.module#ActionSheetPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  components: Componente[] = [
+    { icon: 'american-football', name: 'Action Sheet', redirectTo: '/action-sheet'},
+    { icon: 'appstore', name: 'Alert', redirectTo: '/alert'}
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
