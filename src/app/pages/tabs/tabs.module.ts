7fc5de8883383aeb3dbaa1d96c1fbe1dd406db81
Guido Pontet <guidopontet@gmail.com>
2019-08-03 20:19:02 -0300

    Add ion tabs

---
+++ src/app/pages/tabs/tabs.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'account'
+  },
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'account',
+        loadChildren: '../avatar/avatar.module#AvatarPageModule'
+      },
+      {
+        path: 'popover',
+        loadChildren: '../popover/popover.module#PopoverPageModule'
+      },
+      {
+        path: 'infinite-scroll',
+        loadChildren: '../infinite-scroll/infinite-scroll.module#InfiniteScrollPageModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TabsPage]
+})
+export class TabsPageModule {}
